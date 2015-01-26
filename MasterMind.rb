bc81c06ef0fb34529abaedfeb91dcb320bd763b1
Jason Matthews <kid.bytes@gmail.com>
2015-01-26 11:51:15 -0600

    Added the mode for the player to set a pattern and the computer to guess. Also some bug fixes.

--- MasterMind.rb
+++ MasterMind.rb
@@ -1,7 +1,8 @@
 module MasterMind
 
-  class Game(tries = 12, colors = 6, length = 4)
-    def initialize
+  class Game
+    #initialize a game
+    def initialize(tries = 12, colors = 6, length = 4)
       @mode = nil
       @tries = tries
       @colors = colors
@@ -11,13 +12,32 @@ module MasterMind
       @computer = Computer.new
     end
 
+    #get the mode and start a game loop
     def begin
       get_mode
       if @mode == 0
-        print "Please enter a pattern in the form of a string using #{@pattern_range}'s (i.e. AAAA): "
-        @pattern = get_pattern_from_user
+        @pattern = nil
+        until @pattern && @pattern.length < 9
+          puts "Please enter a pattern in the form of a string using #{@pattern_range}'s"\
+                  " (i.e. AAAA) with a maximum length of 8 characters: "
+          @pattern = get_pattern_from_user
+        end
+
         @pattern_len = @pattern.length
 
+        guess = @computer.initial_guess(@pattern_len, @pattern_range)
+
+        (@tries).times do |n|
+          result = @computer.compare_patterns(guess, @pattern)
+          if win?(result)
+            puts "I guessed the pattern #{guess.join.upcase} in #{n + 1} tries!"
+            break
+          else
+            puts "#{guess.join.upcase} - X: #{result[:right]} | O: #{result[:colors]}"
+            guess = @computer.next_guess(result)
+            puts "Ahh you beat me! I should've known it was #{@patter.join.upcase}" if  n == @tries - 1
+          end
+        end
       elsif @mode == 1
         player_guess_init
 
@@ -29,7 +49,7 @@ module MasterMind
             redo
           end
 
-          result = compare_guess(guess)
+          result = @computer.compare_patterns(guess, @pattern)
           if win?(result)
             puts "You WIN!"
             break
@@ -53,7 +73,7 @@ module MasterMind
         @mode = 1
       else
         puts "That is not a valid selection."
-        return nil
+        @mode = nil
       end
     end
 
@@ -64,30 +84,8 @@ module MasterMind
       input = input.downcase
       input = input.split('')
 
-      input.each { |n| return false unless pattern_range.include?(n.downcase) }
+      input.each { |n| return false unless @pattern_range.include?(n.downcase) }
       input
-    end
-
-    #have the computer class generate a pattern
-    def get_pattern_from_comp
-     @computer.gen_pattern(@pattern_len, @pattern_range)
-    end
-
-    #returns a hash of :right being the amount that are in the right position and color and :colors for the amount
-    #correct colors out of position
-    def compare_guess(guess)
-      right = 0
-      colors = 0
-
-      guess.each_with_index do |n, i|
-        if guess[i] == @pattern[i]
-          right += 1
-        elsif @pattern.include?(guess[i])
-          colors += 1
-        end
-      end
-
-      {right: right, colors: colors}
     end
 
     #displays instructions and generates a pattern
@@ -96,7 +94,7 @@ module MasterMind
                 "you will get an O, for each guess that is correct in color but not position you will get an X. Enter "\
                 "your guess as a string of #{@pattern_len} (i.e. AAAA)"
 
-      @pattern = get_pattern_from_comp
+      @pattern = @computer.gen_pattern(@pattern_len, @pattern_range)
     end
 
     #checks a hash for the win condition
@@ -112,14 +110,52 @@ module MasterMind
       (1..len).map { range.to_a.sample }
     end
 
-    def all_poss
-
+    #populates an array of all possible solutions
+    def populate_solutions(len, range)
+      @solutions = range.to_a.repeated_permutation(len).to_a
     end
 
+    #creates an initial guess with the first half being the first character in range and the second half being the second character
     def initial_guess(len, range)
       first = Array.new(len / 2, range.to_a[0])
       second = Array.new(len - first.length, range.to_a[1])
-      first + second
+      @guess = first + second
+      populate_solutions(len, range)
+      @guess
+    end
+
+    #eliminates all solutions that would not yield the same result and grabs a random 1 from what's left
+    def next_guess(result)
+      @solutions = @solutions.select { |n| compare_patterns(@guess, n) == result }
+      @guess = @solutions.sample
+      @guess
+    end
+
+    #returns a hash of :right being the amount that are in the right position and color and :colors for the amount
+    #correct colors out of position
+    def compare_patterns(pat1, pat2)
+      right = 0
+      colors = 0
+
+      pat1_clone = pat1.clone
+      pat2_clone = pat2.clone
+
+      pat1.each_with_index do |n, i|
+        if pat1[i] == pat2[i]
+          right += 1
+          pat1_clone[i] = nil
+          pat2_clone[i] = nil
+        end
+      end
+
+      pat1_clone.each_with_index do |n, i|
+        if n && pat2_clone.include?(n)
+          colors += 1
+          pat2_clone[pat2_clone.index(n)] = nil
+        end
+      end
+
+      {right: right, colors: colors}
     end
   end
 end
